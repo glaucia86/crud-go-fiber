e983c1eb724cdda609e17f21ab3e44f6008ade5e
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2020-04-25 22:05:15 -0300

    Desenvolvimento do arquivo server.go com Fiber

--- api/go.mod
+++ api/go.mod
@@ -1,4 +1,4 @@
-module api-livro
+module api-book/v1
 
 go 1.14
 
--- api/server.go
+++ api/server.go
@@ -5,8 +5,8 @@ import "github.com/gofiber/fiber"
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) {
-		c.Send("Hello, World!")
+	app.Get("/api", func(c *fiber.Ctx) {
+		c.Send("Sejam bem-vindos(as) a API Go + Fiber + PostGreSQL + Azure!")
 	})
 
 	app.Listen(3000)
